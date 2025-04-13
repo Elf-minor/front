ac59ccf1d2b42ac4a40dcf5f7b91f49bf8816257
Elf-minor <lizamakhno319@gmail.com>
2025-04-13 12:54:43 +0300

    krutilochka

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,32 +1,43 @@
 import React from "react";
+import { FaSpinner } from "react-icons/fa";
 import "./SearchResult.css";
 
-export const SearchResult = ({ result }) => {
+export const SearchResult = ({ result, isLoading=false }) => {
   return (
+
     <div
-      className="search-result"
-      onClick={(e) => alert("Вы выбрали товар: ${result.name}")}
+      className={"search-result ${isLoading ? 'loading' : ''}"}
+      onClick={(e) => !isLoading && alert("Вы выбрали товар: ${result.name}")}
     >
-      <div className="product-image-container">
-        {result.imageUrl && (
-          <img
-            src={result.imageUrl}
-            alt={result.name}
-            className="product-image"
-          />
-        )}
-      </div>
-      <div className="product-info">
-        <h3 className="product-name">{result.name}</h3>
-        <div className="product-details">
-          <span className="product-price">Цена:
-            { `${result.price} ₽`}
-          </span>
+      {isLoading ? (
+        <div className='loading-spinner'>
+          <FaSpinner className='spinner-icon'/>
         </div>
-        {result.article && (
-          <div className="product-article">Артикул: {result.article}</div>
-        )}
-      </div>
+      ) : (
+        <>
+          <div className="product-image-container">
+            {result.imageUrl && (
+              <img
+                src={result.imageUrl}
+                alt={result.name}
+                className="product-image"
+              />
+            )}
+          </div>
+          <div className="product-info">
+            <h3 className="product-name">{result.name}</h3>
+            <div className="product-details">
+              <span className="product-price">Цена:
+                { `${result.price} ₽`}
+              </span>
+            </div>
+            {result.article && (
+            <div className="product-article">Артикул: {result.article}</div>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
+
